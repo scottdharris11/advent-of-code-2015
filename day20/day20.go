45701f71a875e734c1a354f66625b1103530b746
scottdharris <scottdharris11@yahoo.com>
2025-01-18 07:19:38 -0500

    day 20 puzzle part 2

--- day20/day20.go
+++ day20/day20.go
@@ -17,7 +17,7 @@ func solvePart1(goal int) int {
 	start := time.Now().UnixMilli()
 	house := int(math.Sqrt(float64(goal / 10)))
 	for {
-		if presents(house) >= goal {
+		if presents(house, 10, 0) >= goal {
 			break
 		}
 		house++
@@ -27,23 +27,33 @@ func solvePart1(goal int) int {
 	return house
 }
 
-func solvePart2(input int) int {
+func solvePart2(goal int) int {
 	start := time.Now().UnixMilli()
-	result := 0
+	house := int(math.Sqrt(float64(goal / 11)))
+	for {
+		if presents(house, 11, 50) >= goal {
+			break
+		}
+		house++
+	}
 	end := time.Now().UnixMilli()
-	log.Printf("Day 20, Part 2 (%dms): Results = %d", end-start, result)
-	return result
+	log.Printf("Day 20, Part 2 (%dms): House = %d", end-start, house)
+	return house
 }
 
-func presents(house int) int {
+func presents(house int, per int, limit int) int {
 	p := 0
-	limit := int(math.Sqrt(float64(house))) + 1
-	for n := 1; n < limit; n++ {
+	max := int(math.Sqrt(float64(house))) + 1
+	for n := 1; n < max; n++ {
 		if house%n == 0 {
-			p += n * 10
+			if limit == 0 || limit*n >= house {
+				p += n * per
+			}
 			r := house / n
 			if r != n {
-				p += r * 10
+				if limit == 0 || limit*r >= house {
+					p += r * per
+				}
 			}
 		}
 	}
--- day20/day20_test.go
+++ day20/day20_test.go
@@ -15,29 +15,54 @@ func TestSolvePart1(t *testing.T) {
 }
 
 func TestSolvePart2(t *testing.T) {
-	assert.Equal(t, 0, solvePart2(150))
-	assert.Equal(t, 0, solvePart2(29000000))
+	assert.Equal(t, 84, solvePart2(2376))
+	assert.Equal(t, 705600, solvePart2(29000000))
 }
 
 func TestPresents(t *testing.T) {
 	tests := []struct {
 		input  int
 		output int
+		per    int
+		limit  int
 	}{
-		{1, 10},
-		{2, 30},
-		{3, 40},
-		{4, 70},
-		{5, 60},
-		{6, 120},
-		{7, 80},
-		{8, 150},
-		{9, 130},
+		{1, 10, 10, 0},
+		{2, 30, 10, 0},
+		{3, 40, 10, 0},
+		{4, 70, 10, 0},
+		{5, 60, 10, 0},
+		{6, 120, 10, 0},
+		{7, 80, 10, 0},
+		{8, 150, 10, 0},
+		{9, 130, 10, 0},
+		// 84x1 == yes == 924
+		// 1x84 == no  == 0
+		// 42x2 == yes == 462
+		// 2x42 == yes == 22
+		// 28x3 == yes == 308
+		// 3x28 == yes == 33
+		// 21x4 == yes == 231
+		// 4*21 == yes == 44
+		// 14*6 == yes == 154
+		// 6*14 == yes == 66
+		// 12*7 == yes == 132
+		// 7*12 == yes == 77
+		{84, 2453, 11, 50},
+		// 100x1 == yes == 1100
+		// 1x100 == no  == 0
+		// 2x50  == yes == 22
+		// 50x2  == yes == 550
+		// 4x25  == yes == 44
+		// 25x4  == yes == 275
+		// 5*20  == yes == 55
+		// 20*5  == yes == 220
+		// 10x10 == yes == 110
+		{100, 2376, 11, 50},
 	}
 	for _, tt := range tests {
 		tt := tt
 		t.Run(fmt.Sprintf("%d", tt.input), func(t *testing.T) {
-			assert.Equal(t, tt.output, presents(tt.input))
+			assert.Equal(t, tt.output, presents(tt.input, tt.per, tt.limit))
 		})
 	}
 }
