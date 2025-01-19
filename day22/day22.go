05b9bad2da9ea87c735402266db3254abf650a7d
scottdharris <scottdharris11@yahoo.com>
2025-01-19 11:22:49 -0500

    day 22 part 1

--- day22/day22.go
+++ day22/day22.go
@@ -7,41 +7,181 @@ import (
 
 type Puzzle struct{}
 
-var Boss = Player{HitPoints: 58, Damage: 9}
-var Me = Player{HitPoints: 50, Mana: 500}
-
 func (Puzzle) Solve() {
-	solvePart1(Boss, Me)
-	solvePart2(Boss, Me)
+	solvePart1(&Player{HitPoints: 58, Damage: 9}, &Player{HitPoints: 50, Mana: 500})
+	solvePart2(&Player{HitPoints: 58, Damage: 9}, &Player{HitPoints: 50, Mana: 500})
 }
 
-func solvePart1(boss Player, player Player) int {
+func solvePart1(boss *Player, player *Player) int {
+	start := time.Now().UnixMilli()
+	lowest := playTurn(GameState{Boss: boss, Player: player, Cost: 0}, true, -1)
+	end := time.Now().UnixMilli()
+	log.Printf("Day 22, Part 1 (%dms): Lowest Cost = %d", end-start, lowest)
+	return lowest
+}
+
+func solvePart2(boss *Player, player *Player) int {
 	start := time.Now().UnixMilli()
 	result := 0
 	end := time.Now().UnixMilli()
-	log.Printf("Day 21, Part 1 (%dms): Result = %d", end-start, result)
+	log.Printf("Day 22, Part 2 (%dms): Result = %d", end-start, result)
 	return result
 }
 
-func solvePart2(boss Player, player Player) int {
-	start := time.Now().UnixMilli()
-	result := 0
-	end := time.Now().UnixMilli()
-	log.Printf("Day 21, Part 2 (%dms): Result = %d", end-start, result)
-	return result
+func playTurn(state GameState, playerTurn bool, lowest int) int {
+	// check end state
+	if state.Boss.HitPoints <= 0 {
+		// log.Printf("Player wins with moves: %v", strings.Join(state.Moves, ", "))
+		return state.Cost
+	}
+	if state.Player.HitPoints <= 0 {
+		return -1
+	}
+	if lowest >= 0 && state.Cost >= lowest {
+		return -1
+	}
+
+	// apply pre-effects
+	state.Player.shieldEffect()
+	state.Player.poisonEffect(state.Boss)
+	state.Player.rechargeEffect()
+
+	// check boss end state again to see if pre-effects have won
+	if state.Boss.HitPoints <= 0 {
+		// log.Printf("Player wins with moves: %v", strings.Join(state.Moves, ", "))
+		return state.Cost
+	}
+
+	// boss or player turns
+	if playerTurn {
+		for _, move := range state.playerMoves() {
+			cost := playTurn(move, false, lowest)
+			if cost != -1 && (lowest == -1 || cost < lowest) {
+				lowest = cost
+			}
+		}
+	} else {
+		state.Player.HitPoints -= state.Boss.damageTo(*state.Player)
+		cost := playTurn(state, true, lowest)
+		if cost != -1 && (lowest == -1 || cost < lowest) {
+			lowest = cost
+		}
+	}
+	return lowest
 }
 
 type Player struct {
-	HitPoints int
-	Damage    int
-	Armor     int
-	Mana      int
+	HitPoints     int
+	Damage        int
+	Armor         int
+	Mana          int
+	ShieldTurns   int
+	PoisonTurns   int
+	RechargeTurns int
 }
 
-func (a Player) damageTo(b Player) int {
-	d := a.Damage - b.Armor
+func (p *Player) damageTo(b Player) int {
+	d := p.Damage - b.Armor
 	if d < 1 {
 		d = 1
 	}
 	return d
+}
+
+func (p *Player) shieldEffect() {
+	if p.ShieldTurns > 0 {
+		p.ShieldTurns--
+		if p.ShieldTurns == 0 {
+			p.Armor = 0
+		}
+	}
+}
+
+func (p *Player) poisonEffect(b *Player) {
+	if p.PoisonTurns > 0 {
+		p.PoisonTurns--
+		b.HitPoints -= 3
+	}
+}
+
+func (p *Player) rechargeEffect() {
+	if p.RechargeTurns > 0 {
+		p.RechargeTurns--
+		p.Mana += 101
+	}
+}
+
+type GameState struct {
+	Boss   *Player
+	Player *Player
+	Cost   int
+	Moves  []string
+}
+
+func (g GameState) playerMoves() []GameState {
+	var moves []GameState
+	// Magic Missile
+	if g.Player.Mana >= 53 {
+		gs := g.copy()
+		gs.Cost += 53
+		gs.Player.Mana -= 53
+		gs.Boss.HitPoints -= 4
+		gs.Moves = append(gs.Moves, "Player casts Magic Missile")
+		moves = append(moves, gs)
+	}
+	// Drain
+	if g.Player.Mana >= 73 {
+		gs := g.copy()
+		gs.Cost += 73
+		gs.Player.Mana -= 73
+		gs.Boss.HitPoints -= 2
+		gs.Player.HitPoints += 2
+		gs.Moves = append(gs.Moves, "Player casts Drain")
+		moves = append(moves, gs)
+	}
+	// Shield
+	if g.Player.Mana >= 113 && g.Player.ShieldTurns == 0 {
+		gs := g.copy()
+		gs.Cost += 113
+		gs.Player.Mana -= 113
+		gs.Player.Armor += 7
+		gs.Player.ShieldTurns = 6
+		gs.Moves = append(gs.Moves, "Player casts Shield")
+		moves = append(moves, gs)
+	}
+	// Poison
+	if g.Player.Mana >= 173 && g.Player.PoisonTurns == 0 {
+		gs := g.copy()
+		gs.Cost += 173
+		gs.Player.Mana -= 173
+		gs.Player.PoisonTurns = 6
+		gs.Moves = append(gs.Moves, "Player casts Poison")
+		moves = append(moves, gs)
+	}
+	// Recharge
+	if g.Player.Mana >= 229 && g.Player.RechargeTurns == 0 {
+		gs := g.copy()
+		gs.Cost += 229
+		gs.Player.Mana -= 229
+		gs.Player.RechargeTurns = 5
+		gs.Moves = append(gs.Moves, "Player casts Recharge")
+		moves = append(moves, gs)
+	}
+	return moves
+}
+
+func (g GameState) copy() GameState {
+	b := Player{HitPoints: g.Boss.HitPoints, Damage: g.Boss.Damage}
+	p := Player{
+		HitPoints:     g.Player.HitPoints,
+		Damage:        g.Player.Damage,
+		Armor:         g.Player.Armor,
+		Mana:          g.Player.Mana,
+		ShieldTurns:   g.Player.ShieldTurns,
+		PoisonTurns:   g.Player.PoisonTurns,
+		RechargeTurns: g.Player.RechargeTurns,
+	}
+	m := make([]string, len(g.Moves))
+	copy(m, g.Moves)
+	return GameState{Boss: &b, Player: &p, Cost: g.Cost, Moves: m}
 }
--- day22/day22_test.go
+++ day22/day22_test.go
@@ -7,11 +7,12 @@ import (
 )
 
 func TestSolvePart1(t *testing.T) {
-	assert.Equal(t, 0, solvePart1(Player{HitPoints: 13, Damage: 8}, Player{HitPoints: 10, Mana: 250}))
-	assert.Equal(t, 0, solvePart1(Boss, Me))
+	assert.Equal(t, 226, solvePart1(&Player{HitPoints: 13, Damage: 8}, &Player{HitPoints: 10, Mana: 250}))
+	assert.Equal(t, 641, solvePart1(&Player{HitPoints: 14, Damage: 8}, &Player{HitPoints: 10, Mana: 250}))
+	assert.Equal(t, 1269, solvePart1(&Player{HitPoints: 58, Damage: 9}, &Player{HitPoints: 50, Mana: 500}))
 }
 
 func TestSolvePart2(t *testing.T) {
-	assert.Equal(t, 0, solvePart2(Player{HitPoints: 13, Damage: 8}, Player{HitPoints: 10, Mana: 250}))
-	assert.Equal(t, 0, solvePart2(Boss, Me))
+	assert.Equal(t, 0, solvePart2(&Player{HitPoints: 13, Damage: 8}, &Player{HitPoints: 10, Mana: 250}))
+	assert.Equal(t, 0, solvePart2(&Player{HitPoints: 58, Damage: 9}, &Player{HitPoints: 50, Mana: 500}))
 }
