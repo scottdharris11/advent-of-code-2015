d457d1483c5a393590c453a0907915a300775c5f
scottdharris <scottdharris11@yahoo.com>
2025-01-20 18:41:56 -0500

    day 24 part 1 wrong answer

--- day24/day24.go
+++ day24/day24.go
@@ -17,7 +17,8 @@ func (Puzzle) Solve() {
 
 func solvePart1(weights []int) int {
 	start := time.Now().UnixMilli()
-	best := balanced(NewConfiguration(weights), nil)
+	best := balanced(weights, NewConfiguration(weights, 3), nil)
+	log.Printf("Best: %+v", *best)
 	end := time.Now().UnixMilli()
 	log.Printf("Day 24, Part 1 (%dms): Result = %d", end-start, best.quantum)
 	return best.quantum
@@ -31,7 +32,7 @@ func solvePart2(weights []int) int {
 	return result
 }
 
-func balanced(c *Configuration, best *Configuration) *Configuration {
+func balanced(weights []int, c *Configuration, best *Configuration) *Configuration {
 	if c.boxesRemaining == 0 {
 		if c.balance == 0 && c.better(best) {
 			best = c.copy()
@@ -41,17 +42,19 @@ func balanced(c *Configuration, best *Configuration) *Configuration {
 	if !c.stillPossible(best) {
 		return best
 	}
-	box := c.boxesRemaining - 1
-	for container := 1; container < 4; container++ {
+	box := weights[c.boxesRemaining-1]
+	for container := 0; container < 3; container++ {
 		c.boxToContainer(box, container)
-		best = balanced(c, best)
+		best = balanced(weights, c, best)
 		c.removeLastBoxFromContainer(container)
 	}
 	return best
 }
 
-func NewConfiguration(boxes []int) *Configuration {
-	c := &Configuration{boxes: boxes, boxesRemaining: len(boxes), quantum: 1}
+func NewConfiguration(boxes []int, containerCount int) *Configuration {
+	containers := make([][]int, containerCount)
+	weights := make([]int, containerCount)
+	c := &Configuration{boxesRemaining: len(boxes), containers: containers, weights: weights, quantum: 1}
 	for _, b := range boxes {
 		c.weightToDistribute += b
 	}
@@ -59,15 +62,10 @@ func NewConfiguration(boxes []int) *Configuration {
 }
 
 type Configuration struct {
-	boxes              []int
 	weightToDistribute int
 	boxesRemaining     int
-	c1                 []int
-	c1Weight           int
-	c2                 []int
-	c2Weight           int
-	c3                 []int
-	c3Weight           int
+	containers         [][]int
+	weights            []int
 	balance            int
 	quantum            int
 }
@@ -79,10 +77,12 @@ func (c *Configuration) stillPossible(best *Configuration) bool {
 	if best == nil {
 		return true
 	}
-	if c.c1Weight > best.c1Weight || c.c2Weight > best.c2Weight || c.c3Weight > best.c3Weight {
-		return false
+	for i, weight := range c.weights {
+		if weight > best.weights[i] {
+			return false
+		}
 	}
-	if len(c.c1) > len(best.c1) {
+	if len(c.containers[0]) > len(best.containers[0]) {
 		return false
 	}
 	if c.quantum > best.quantum {
@@ -95,10 +95,10 @@ func (c *Configuration) better(best *Configuration) bool {
 	if best == nil {
 		return true
 	}
-	if len(c.c1) > len(best.c1) {
+	if len(c.containers[0]) > len(best.containers[0]) {
 		return false
 	}
-	if len(c.c1) < len(best.c1) {
+	if len(c.containers[0]) < len(best.containers[0]) {
 		return true
 	}
 	return c.quantum < best.quantum
@@ -106,19 +106,11 @@ func (c *Configuration) better(best *Configuration) bool {
 
 func (c *Configuration) boxToContainer(box int, container int) {
 	c.boxesRemaining--
-	weight := c.boxes[box]
-	c.weightToDistribute -= weight
-	switch container {
-	case 1:
-		c.c1 = append(c.c1, weight)
-		c.c1Weight += weight
-		c.quantum *= weight
-	case 2:
-		c.c2 = append(c.c2, weight)
-		c.c2Weight += weight
-	case 3:
-		c.c3 = append(c.c3, weight)
-		c.c3Weight += weight
+	c.weightToDistribute -= box
+	c.containers[container] = append(c.containers[container], box)
+	c.weights[container] += box
+	if container == 0 {
+		c.quantum *= box
 	}
 	c.updateBalance()
 }
@@ -126,49 +118,40 @@ func (c *Configuration) boxToContainer(box int, container int) {
 func (c *Configuration) removeLastBoxFromContainer(container int) {
 	c.boxesRemaining++
 	var weight int
-	switch container {
-	case 1:
-		idx := len(c.c1) - 1
-		weight = c.c1[idx]
-		c.c1 = c.c1[:idx]
-		c.c1Weight -= weight
+	idx := len(c.containers[container]) - 1
+	weight = c.containers[container][idx]
+	c.containers[container] = c.containers[container][:idx]
+	c.weights[container] -= weight
+	if container == 0 {
 		c.quantum /= weight
-	case 2:
-		idx := len(c.c2) - 1
-		weight = c.c2[idx]
-		c.c2 = c.c2[:idx]
-		c.c2Weight -= weight
-	case 3:
-		idx := len(c.c3) - 1
-		weight = c.c3[idx]
-		c.c3 = c.c3[:idx]
-		c.c3Weight -= weight
 	}
 	c.weightToDistribute += weight
 	c.updateBalance()
 }
 
 func (c *Configuration) updateBalance() {
-	c.balance = int(math.Abs(float64(c.c1Weight-c.c2Weight)) + math.Abs(float64(c.c1Weight-c.c3Weight)))
+	c1Weight := c.weights[0]
+	balance := 0
+	for i := 1; i < len(c.weights); i++ {
+		balance += int(math.Abs(float64(c1Weight - c.weights[i])))
+	}
+	c.balance = balance
 }
 
 func (c *Configuration) copy() *Configuration {
-	c1 := make([]int, len(c.c1))
-	copy(c1, c.c1)
-	c2 := make([]int, len(c.c2))
-	copy(c2, c.c2)
-	c3 := make([]int, len(c.c3))
-	copy(c3, c.c3)
+	cnt := len(c.containers)
+	containers := make([][]int, cnt)
+	weights := make([]int, cnt)
+	for i := 0; i < cnt; i++ {
+		containers[i] = make([]int, len(c.containers[i]))
+		copy(containers[i], c.containers[i])
+		weights[i] = c.weights[i]
+	}
 	return &Configuration{
-		boxes:              c.boxes,
 		weightToDistribute: c.weightToDistribute,
 		boxesRemaining:     c.boxesRemaining,
-		c1:                 c1,
-		c1Weight:           c.c1Weight,
-		c2:                 c2,
-		c2Weight:           c.c2Weight,
-		c3:                 c3,
-		c3Weight:           c.c3Weight,
+		containers:         containers,
+		weights:            weights,
 		balance:            c.balance,
 		quantum:            c.quantum,
 	}
