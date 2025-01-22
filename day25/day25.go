33fc842d445a28efa74d5140d1108dd35379062c
scottdharris <scottdharris11@yahoo.com>
2025-01-21 20:13:59 -0500

    day 24 part 1

---
+++ day25/day25.go
@@ -0,0 +1,44 @@
+package day25
+
+import (
+	"log"
+	"time"
+)
+
+type Puzzle struct{}
+
+func (Puzzle) Solve() {
+	solvePart1(2978, 3083)
+}
+
+func solvePart1(row int, col int) int {
+	start := time.Now().UnixMilli()
+	code := code(codeNumber(row, col))
+	end := time.Now().UnixMilli()
+	log.Printf("Day 24, Part 1 (%dms): Code = %d", end-start, code)
+	return code
+}
+
+func codeNumber(row int, col int) int {
+	n := 1
+	for i := 2; i < col+1; i++ {
+		n += i
+	}
+	for i := 2; i < row+1; i++ {
+		n += i + col - 2
+	}
+	return n
+}
+
+func code(codeNumber int) int {
+	c := 20151125
+	for i := 1; i < codeNumber; i++ {
+		c = nextCode(c)
+	}
+	return c
+}
+
+func nextCode(prev int) int {
+	c := prev * 252533
+	return c % 33554393
+}
---
+++ day25/day25_test.go
@@ -0,0 +1,71 @@
+package day25
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSolvePart1(t *testing.T) {
+	assert.Equal(t, 9380097, solvePart1(4, 4))
+	assert.Equal(t, 27995004, solvePart1(6, 6))
+	assert.Equal(t, 2650453, solvePart1(2978, 3083))
+}
+
+func TestCodeNumber(t *testing.T) {
+	tests := []struct {
+		row      int
+		col      int
+		expected int
+	}{
+		{1, 1, 1},
+		{2, 1, 2},
+		{3, 2, 8},
+		{4, 3, 18},
+		{6, 1, 16},
+		{1, 5, 15},
+		{2, 5, 20},
+		{1, 6, 21},
+	}
+	for _, tt := range tests {
+		tt := tt
+		t.Run(fmt.Sprintf("%d/%d", tt.row, tt.col), func(t *testing.T) {
+			assert.Equal(t, tt.expected, codeNumber(tt.row, tt.col))
+		})
+	}
+}
+
+func TestCode(t *testing.T) {
+	tests := []struct {
+		codeNumber int
+		expected   int
+	}{
+		{1, 20151125},
+		{2, 31916031},
+		{6, 17289845},
+	}
+	for _, tt := range tests {
+		tt := tt
+		t.Run(fmt.Sprintf("%d", tt.codeNumber), func(t *testing.T) {
+			assert.Equal(t, tt.expected, code(tt.codeNumber))
+		})
+	}
+}
+
+func TestNextCode(t *testing.T) {
+	tests := []struct {
+		prev     int
+		expected int
+	}{
+		{20151125, 31916031},
+		{31916031, 18749137},
+		{18749137, 16080970},
+	}
+	for _, tt := range tests {
+		tt := tt
+		t.Run(fmt.Sprintf("%d", tt.prev), func(t *testing.T) {
+			assert.Equal(t, tt.expected, nextCode(tt.prev))
+		})
+	}
+}
